ddcc2da1320be93e99707843b3016af46912faed
Saurabh Singh <saurabh.s99100@gmail.com>
2022-02-21 11:29:33 +0530

    Optimize ALU
    Synthesis on Yosys v0.14+6; for spartan6
    === Alu ===
    
       Number of wires:                247
       Number of wire bits:           1088
       Number of public wires:           9
       Number of public wire bits:     259
       Number of memories:               0
       Number of memory bits:            0
       Number of processes:              0
       Number of cells:                480
         CARRY4                         16
         IBUF                           67
         INV                            31
         LUT1                            2
         LUT2                            6
         LUT3                           11
         LUT4                           85
         LUT5                           41
         LUT6                          130
         MUXF7                          54
         MUXF8                           5
         OBUF                           32
    
       Estimated number of LCs:        267

--- rtl/core/Alu.v
+++ rtl/core/Alu.v
@@ -27,22 +27,68 @@ module Alu
     output  reg     [31:0]  result_o
 );
 
-    wire signed [31:0] A_s = a_i;
+    wire sel_add = (sel_i == `ALU_FUNC_ADD);
+    wire sel_sub = (sel_i == `ALU_FUNC_SUB);
+    wire sel_xor = (sel_i == `ALU_FUNC_XOR);
+    wire sel_or  = (sel_i == `ALU_FUNC_OR);
+    wire sel_and = (sel_i == `ALU_FUNC_AND);
+    wire sel_sll = (sel_i == `ALU_FUNC_SLL);
+    wire sel_srl = (sel_i == `ALU_FUNC_SRL);
+    wire sel_sra = (sel_i == `ALU_FUNC_SRA);
 
+    // Result of arithmetic calculations (ADD/SUB)
+    wire [31:0] arith_result = a_i + (sel_sub ? ((~b_i)+1) : b_i);
+
+    // Bitreverse
+    function [31:0] reverse;
+        input [31:0]    ain;
+        integer i;
+        begin
+            for(i=0; i<32; i=i+1) begin
+                reverse[32-1-i] = ain[i];
+            end
+        end
+    endfunction
+    
+    // Input to the universal shifter
+    reg signed [32:0] shift_input;
     always @(*) begin
-        case(sel_i)
-
-            `ALU_FUNC_ADD:  result_o = a_i + b_i;
-            `ALU_FUNC_SUB:  result_o = a_i - b_i; 
-            `ALU_FUNC_XOR:  result_o = a_i ^ b_i;
-            `ALU_FUNC_OR:   result_o = a_i | b_i;
-            `ALU_FUNC_AND:  result_o = a_i & b_i;
-            `ALU_FUNC_SLL:  result_o = a_i << b_i[4:0];
-            `ALU_FUNC_SRL:  result_o = a_i >> b_i[4:0];
-            `ALU_FUNC_SRA:  result_o = A_s >>> b_i[4:0];
-
-            default:    result_o = 32'd0;
-        endcase
+        if (sel_srl)
+            shift_input = {1'b0, a_i};
+        else if (sel_sra)
+            shift_input = {a_i[31], a_i};
+        else // if (sel_sll) // this case includes "sel_sll"
+            shift_input = {1'b0, reverse(a_i)};
     end
+
+    /* verilator lint_off UNUSED */
+    wire [32:0] shift_output = shift_input >>> b_i[4:0];    // Universal shifter
+    /* verilator lint_on UNUSED */
+
+
+    // output of universal shifter
+    reg [31:0] final_shift_output;
+    always @(*) begin
+        if (sel_sll)
+            final_shift_output = reverse(shift_output[31:0]);
+        else
+            final_shift_output = shift_output[31:0];
+    end
+    
+    // Final output mux
+    always @(*) begin
+        if (sel_add | sel_sub)
+            result_o = arith_result;
+        else if (sel_sll | sel_srl | sel_sra)
+            result_o = final_shift_output;
+        else if (sel_xor)
+            result_o = a_i ^ b_i;
+        else if (sel_or)
+            result_o = a_i | b_i;
+        else if (sel_and)
+            result_o = a_i & b_i;
+        else
+            result_o = arith_result;
+    end        
 
 endmodule
