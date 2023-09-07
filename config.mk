d3bfd0f0b84059530b9d866fad097063e2215085
Saurabh Singh <saurabh.s99100@gmail.com>
2023-09-06 20:24:20 -0400

    remove config.mk
    - sourceme script defines VERILATOR_PATH which is used by makefile

--- config.mk
+++
@@ -1,4 +0,0 @@
-# This should point to verilator include directory
-VERILATOR_INCLUDE_PATH := /opt/verilator/share/verilator/include
-# VERILATOR_INCLUDE_PATH := /usr/share/verilator/include
-# VERILATOR_INCLUDE_PATH := /tools/vlsi/verilator/$(shell verilator --version | awk '{print $$2}')/share/verilator/include
