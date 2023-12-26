5ce3a3cbe9aa74a1dfb450dd04ccdd2cbe05ecdb
Saurabh Singh <saurabh.s99100@gmail.com>
2023-12-26 02:21:51 -0500

    minor changes in examples

--- sw/examples/banner/banner.c
+++ sw/examples/banner/banner.c
@@ -1,6 +1,8 @@
 #include "platform.h"
 #include <stdio.h>
 #include <csr.h>
+#include  <gpio.h>
+#include <time.h>
 
 char * banner = 
 "\n"
@@ -28,15 +30,24 @@ char * banner =
 " /_/ |_/___//____/\\____/  |___/   \\__,_/\\__/\\____/_/ /_/ /_/  \n"
 "/=========By: Saurabh Singh (saurabh.s99100@gmail.com)====/\n\n";
 
+print_isastring(){
+    uint32_t misa = CSR_read(CSR_MISA);
+    puts("RV32I");
+    if(bitcheck(misa, 12))  putchar('M');
+    if(bitcheck(misa, 0))   putchar('A');
+    if(bitcheck(misa, 5))   putchar('F');
+    if(bitcheck(misa, 3))   putchar('D');
+    if(bitcheck(misa, 2))   putchar('C');   
+}
+
 int main()
 {
     serial_init(UART_BAUD_115200);
     puts(banner);
     printf("CPU      : RISC-V Atom @ %d Hz\n", CLK_FREQ);
-    puts("Arch     : RV32I - little endian\n");
+    puts("Arch     : "); print_isastring(); puts(" - little endian\n");
     printf("CODE RAM : 0x%08x (%d bytes)\n", (unsigned)CODERAM_BASE, (unsigned)CODERAM_SIZE);
     printf("DATA RAM : 0x%08x (%d bytes)\n", (unsigned)DATARAM_BASE, (unsigned)DATARAM_SIZE);
     puts("Exiting...\n");
-    asm volatile("ecall");
     return 0;
 }
